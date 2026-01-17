f00666e561dca9a0fcf6f109ff85dfd46955553e
Anonna <136353692+0xanonna111@users.noreply.github.com>
2026-01-17 21:41:19 +0600

    Create web3.js

---
+++ web3.js
@@ -0,0 +1,12 @@
+import { ethers } from "ethers";
+
+export async function connectWallet() {
+  if (!window.ethereum) {
+    alert("Please install MetaMask");
+    return;
+  }
+
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  await provider.send("eth_requestAccounts", []);
+  alert("Wallet connected");
+}
